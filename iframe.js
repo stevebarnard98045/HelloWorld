df572ec40974b13aab4a3e017ac24eae31574cf3
stevebarnard98045 <101280595+stevebarnard98045@users.noreply.github.com>
2023-06-06 22:03:02 -0700

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,134 +1,75 @@
-import { i, _ as _decorate, s, e, y, a as e$1 } from './query-assigned-elements-5558b813.js';
-import { i as i$1 } from './style-map-82785fc5.js';
-import './directive-2bb7789e.js';
+import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
-const baseStyle = i`
-  :host {
-    height: 100%;
-    width: 100%;
-    display: block;
-  }
-
-  .frame {
-    display: inline-block;
-    height: 100%;
-    width: 100%;
-    background-color: transparent;
-    border: none;
-  }
-`;
-const styles = [baseStyle];
-
-let NintexSampleIframe = _decorate([e$1('form-plugin-iframe')], function (_initialize, _LitElement) {
-  class NintexSampleIframe extends _LitElement {
-    constructor(...args) {
-      super(...args);
-      _initialize(this);
-    }
-  }
-  return {
-    F: NintexSampleIframe,
-    d: [{
-      kind: "field",
-      static: true,
-      key: "styles",
-      value() {
-        return styles;
+export class SampleIframe extends LitElement {
+    // Define scoped styles right with your component, in plain CSS
+    static styles = css`  //Add custom CSS. See https://help.nintex.com/en-US/formplugins/Reference/Style.htm
+      :host {
+        height: 100%;
+        width: 100%;
+        display: block;
       }
-    }, {
-      kind: "field",
-      decorators: [e()],
-      key: "name",
-      value: void 0
-    }, {
-      kind: "field",
-      decorators: [e()],
-      key: "description",
-      value: void 0
-    }, {
-      kind: "field",
-      decorators: [e()],
-      key: "src",
-      value() {
-        return 'https://www.wikipedia.org/';
-      }
-    }, {
-      kind: "field",
-      decorators: [e({
-        type: Number
-      })],
-      key: "height",
-      value() {
-        return 500;
-      }
-    }, {
-      kind: "method",
-      static: true,
-      key: "getMetaConfig",
-      value:
-      // Define scoped styles right with your component, in plain CSS
 
-      function getMetaConfig() {
+      .frame {
+        display: inline-block;
+        height: 100%;
+        width: 100%;
+        background-color: transparent;
+        border: none;
+      }
+    `;
+
+    static getMetaConfig() {
         // plugin contract information
         return {
-          controlName: 'IFrame-new',
-          fallbackDisableSubmit: false,
-          description: 'IFrame component which can render url view with the frame',
-          iconUrl: 'one-line-text',
-          groupName: 'Visual',
-          version: '1.3',
-          properties: {
-            src: {
-              type: 'string',
-              title: 'Source URL',
-              description: 'URL of the iframe, please note many sites block been rendered in iframes',
-              defaultValue: 'https://www.wikipedia.org/'
+            controlName: 'IFrame-new',
+            fallbackDisableSubmit: false,
+            description: 'IFrame component which can render url view with the frame',
+            iconUrl: "one-line-text",
+            groupName: 'Visual',
+            version: '1.3',
+            properties: { //Custom configuration fields. See https://help.nintex.com/en-US/formplugins/Reference/CustomField.htm
+                src: {
+                    type: 'string',
+                    title: 'Source URL',
+                    description: 'URL of the iframe, please note many sites block been rendered in iframes'
+                },
+                height: {
+                    type: 'string',
+                    title: 'Height',
+                    description: 'Height of the component',
+                }
             },
-            height: {
-              type: 'string',
-              title: 'Height',
-              description: 'Height of the component',
-              defaultValue: 500
-            },
-            frameTitle: {
-              type: 'string',
-              title: 'Frame Title',
-              description: 'IFrame Title'
+            standardProperties: {
+                readOnly: true,  //Add a read-only mode. See https://help.nintex.com/en-US/formplugins/Reference/ReadOnly.htm
+                required: true,
+                description: true,
             }
-          },
-          standardProperties: {
-            readOnly: true,
-            required: true,
-            description: true
-          }
         };
-      }
+    }
 
-      // Render the UI as a function of component state
-    }, {
-      kind: "method",
-      key: "render",
-      value: function render() {
-        console.log('Props', {
-          name: this.name,
-          description: this.description,
-          src: this.src,
-          height: this.height
-        });
-        let styles = {
-          height: this.height + 'px'
-        };
-        return y` <iframe
-      class="frame"
-      style=${i$1(styles)}
-      .name="${this.name}"
-      allow="geolocation *; microphone; camera"
-      .title="${this.description}"
-      src="${this.src}"
-    ></iframe>`;
-      }
-    }]
-  };
-}, s);
+    static properties = {
+        name: 'Hello',
+        title: 'Hello',
+        src: 'https://www.wikipedia.org/',
+        height: '100%'
+    }
 
-export { NintexSampleIframe };
+    // Render the UI as a function of component state
+    render() {
+        let styles = {height: this.height};
+
+        return html`
+            <iframe
+                    class="frame"
+                    style=${styleMap(styles)}
+                    name=${this.name}
+                    allow="geolocation *; microphone; camera"
+                    title=${this.title}
+                    src=${this.src}
+            ></iframe>`;
+    }
+}
+
+// registering the web component.
+const elementName = 'sample-iframe';
+customElements.define(elementName, SampleIframe);
